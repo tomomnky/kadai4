1d7fb7de7e9e6cf3cf768b93c23d776d72b98f7e
g16928nt <g16928nt@gm.tsuda.ac.jp>
2018-04-26 14:03:17 +0900

    kadai3-2

---
+++ test/junit/kadai/SnakeCamelUtillTest.java
@@ -0,0 +1,14 @@
+package junit.kadai;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelUtillTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
